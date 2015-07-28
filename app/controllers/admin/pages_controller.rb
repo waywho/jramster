ab7400799091d4cdfba69f99c8e8d3889ca06385
WeiHsi Hu <weihsi.hu@gmail.com>
2015-07-28 12:31:00 +0000

    simple form added

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,13 +1,18 @@
 class Admin::PagesController < ApplicationController
 	before_action :authenticate_user!
-	
+
 	def index
+		@pages = Page.all
 	end
 
 	def show	
 	end
 
 	def new
+	end
+
+	def edit
+		@page = Page.friendly.find(params[:id])
 	end
 
 	def create
